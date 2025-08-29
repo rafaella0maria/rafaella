2515b75fb72f3de1479565b981a51bbb99accd65
rafaella0maria <rafaellamariaa0521@gmail.com>
2025-08-29 19:05:08 -0300

    Add files via upload

---
+++ arquivo.js
@@ -0,0 +1,4 @@
+function mostrarMensagem() {
+    const mensagem = document.getElementById("mensagem");
+    mensagem.innerText = "MPB, é um gênero que carrega a alma e a diversidade cultural do Brasil. Suas raízes se estendem desde a influência dos ritmos europeus, africanos e indígenas, que, ao longo dos séculos, se fundiram para criar um som único e inconfundível.";
+}
